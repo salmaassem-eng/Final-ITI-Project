6d71ae98485b781c3ca7598ea98b063e9423f87f
RewaaGaber <rola.gaber460@gmail.com>
2024-08-16 21:04:55 +0300

    edit&add product page

--- src/Components/CRUD/EditProduct.jsx
+++ src/Components/CRUD/EditProduct.jsx
@@ -1,11 +1,11 @@
 import axios from "axios";
 import React, { useId, useState, useRef, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import { Alert } from "@mui/material";
 
 function EditProducts() {
   const inputRef = useRef(null);
-
-  let [prdct, setPrdct] = useState({
+  const [prdct, setPrdct] = useState({
     id: useId(),
     title: "",
     price: 0,
@@ -16,147 +16,165 @@ function EditProducts() {
       "https://www.energyfit.com.mk/wp-content/plugins/ap_background/images/default/default_large.png",
     category: "",
   });
-  let navigator = useNavigate();
-  let { id } = useParams();
+  const [success, setSuccess] = useState(false);
+  const navigator = useNavigate();
+  const { id } = useParams();
+
   useEffect(() => {
-    let prodct = () => {
-      axios
-        .get(`http://localhost:5000/products/${id}`)
-        .then((res) =>
-          setPrdct({
-            id: res.data.id,
-            title: res.data.title,
-            price: res.data.price,
-            discountPercentage: res.data.discountPercentage,
-            description: res.data.description,
-            stock: res.data.stock,
-            thumbnail: res.data.thumbnail,
-            category: res.data.category,
-          })
-        )
-        .catch((err) => console.log(err));
+    const fetchProduct = async () => {
+      try {
+        const res = await axios.get(`http://localhost:5000/products/${id}`);
+        setPrdct(res.data);
+      } catch (err) {
+        console.log(err);
+      }
     };
-    prodct();
+    fetchProduct();
   }, [id]);
-  let handleChange = (e) => {
-    let { name, value } = e.target;
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
     setPrdct((prev) => ({
       ...prev,
       [name]: value,
     }));
   };
 
-  let handleSubmit = (e) => {
-    axios.patch(`http://localhost:5000/products/${prdct.id}`, prdct);
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    navigator("/shop");
+    try {
+      await axios.patch(`http://localhost:5000/products/${prdct.id}`, prdct);
+      setSuccess(true);
+      setTimeout(() => {
+        setSuccess(false);
+        navigator("/shop");
+      }, 2000);
+    } catch (err) {
+      console.log(err);
+    }
   };
+
   return (
-    <div
-      className="mt-3 p-5 container w-75 mb-5"
-      style={{
-        backgroundColor: " rgba(203,203,203,.6) ",
-        borderRadius: "10px",
-        boxShadow: " rgba(0, 0, 0, 0.35) 0px 5px 15px",
-      }}
-    >
-      <h1 className="text-center pb-4" style={{ color: " #ae7d34 " }}>
-        Edit Product
-      </h1>
-      <form onSubmit={handleSubmit} className="p-4">
-        <div className="form-floating mb-3  ">
-          <input
-            type="text"
-            className="form-control "
-            id="title"
-            name="title"
-            value={prdct.title}
-            onChange={handleChange}
-            ref={inputRef}
-          />
-          <label htmlFor="title">Name</label>
-        </div>
-        <div className="form-floating mb-3">
-          <input
-            type="number"
-            className="form-control"
-            id="price"
-            name="price"
-            value={prdct.price}
-            onChange={handleChange}
-          />
-          <label htmlFor="price">Price</label>
-        </div>
-        <div className="form-floating mb-3">
-          <input
-            type="number"
-            className="form-control"
-            id="discountPercentage"
-            name="discountPercentage"
-            value={prdct.discountPercentage}
-            onChange={handleChange}
-          />
-          <label htmlFor="discountPercentage">Discount Percentage</label>
-        </div>
-        <div className="form-floating mb-3">
-          <input
-            type="number"
-            className="form-control"
-            id="stock"
-            name="stock"
-            value={prdct.stock}
-            onChange={handleChange}
-          />
-          <label htmlFor="stock">Quantity</label>
-        </div>
-        <div className=" form-floating mb-3">
-          <input
-            type="text"
-            className="form-control"
-            id="category"
-            name="category"
-            value={prdct.category}
-            onChange={handleChange}
-          />
-          <label htmlFor="category">Category</label>
-        </div>
-        <div className="form-floating mb-3">
-          <input
-            type="text"
-            className="form-control"
-            id="description"
-            name="description"
-            value={prdct.description}
-            onChange={handleChange}
-          />
-          <label htmlFor="description">Description</label>
-        </div>
-        <div className=" form-floating mb-3">
-          <input
-            type="text"
-            className="form-control"
-            id="thumbnail"
-            name="thumbnail"
-            value={prdct.thumbnail}
-            onChange={handleChange}
-          />
-          <label htmlFor="thumbnail">Image Url</label>
-        </div>
-        <div className="mb-3">
-          <div className="text-center">
-            <button
-              className="btn btn-info text-light px-5 py-2 b-0 "
-              style={{
-                backgroundColor: "#354c5f",
-                cursor: "pointer",
-                boxShadow: "rgba(0, 0, 0, 0.35) 0px 2px 10px",
-              }}
-            >
-              Edit
-            </button>
+    <div className="container w-50 p-5">
+      <div className="p-4 shadow-lg">
+        <h1 className="text-center">Edit Product</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="title" className="form-label">
+              Name
+            </label>
+            <input
+              required
+              type="text"
+              className="form-control"
+              id="title"
+              name="title"
+              value={prdct.title}
+              onChange={handleChange}
+              ref={inputRef}
+              placeholder="Enter name"
+            />
           </div>
-        </div>
-      </form>
+          <div className="mb-3">
+            <label htmlFor="price" className="form-label">
+              Price
+            </label>
+            <input
+              type="number"
+              className="form-control"
+              id="price"
+              name="price"
+              value={prdct.price}
+              onChange={handleChange}
+              placeholder="Enter price"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="discountPercentage" className="form-label">
+              Discount Percentage
+            </label>
+            <input
+              required
+              type="number"
+              className="form-control"
+              id="discountPercentage"
+              name="discountPercentage"
+              value={prdct.discountPercentage}
+              onChange={handleChange}
+              placeholder="Enter discount percentage"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="stock" className="form-label">
+              Quantity
+            </label>
+            <input
+              required
+              type="number"
+              className="form-control"
+              id="stock"
+              name="stock"
+              value={prdct.stock}
+              onChange={handleChange}
+              placeholder="Enter quantity"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="category" className="form-label">
+              Category
+            </label>
+            <input
+              required
+              type="text"
+              className="form-control"
+              id="category"
+              name="category"
+              value={prdct.category}
+              onChange={handleChange}
+              placeholder="Enter category"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="description" className="form-label">
+              Description
+            </label>
+            <input
+              required
+              type="text"
+              className="form-control"
+              id="description"
+              name="description"
+              value={prdct.description}
+              onChange={handleChange}
+              placeholder="Enter description"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="thumbnail" className="form-label">
+              Image Url
+            </label>
+            <input
+              required
+              type="text"
+              className="form-control"
+              id="thumbnail"
+              name="thumbnail"
+              value={prdct.thumbnail}
+              onChange={handleChange}
+              placeholder="Enter image url"
+            />
+          </div>
+          <button type="submit" className="btn btn-primary w-50">
+            Save
+          </button>
+        </form>
+        {success && (
+          <Alert severity="success" className="mt-3">
+            Product updated successfully!
+          </Alert>
+        )}
+      </div>
     </div>
   );
 }
