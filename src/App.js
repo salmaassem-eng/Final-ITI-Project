a6b46f439638e80b0888574bfc4124ab55c35b70
Salma Assem <salmaassem95@gmail.com>
2024-08-17 19:57:53 +0200

    test

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ import Notfound from "./Components/NotFound";
 import ScrollToTop from './Components/ScrollToTop.jsx';
 
 function App() {
-  const [isLogin, setIsLogin] = useState(localStorage.getItem("username"));
+  const [isLogin, setIsLogin] = useState(localStorage.getItem("id"));
 
   return (
     <Router>
