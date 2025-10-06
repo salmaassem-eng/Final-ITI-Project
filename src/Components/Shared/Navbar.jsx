2008ae418bfc8541be5d5676bb77696e6f86cf06
SalmaAseem <111656512+salmaassem-eng@users.noreply.github.com>
2025-10-06 22:31:51 +0300

    Update Navbar.jsx

--- src/Components/Shared/Navbar.jsx
+++ src/Components/Shared/Navbar.jsx
@@ -99,8 +99,7 @@ export default function Navbar({ isLogin, setIsLogin }) {
             ) : (
               ""
             )}
-            
-<<<<<<< HEAD
+
              {/* handle signin and signout */}
             {!isLogin ? (
               <>
@@ -132,8 +131,6 @@ export default function Navbar({ isLogin, setIsLogin }) {
                 </Link>
               </li>
             )}
-=======
->>>>>>> main
             <li className="nav-item">
               <Link
                 to="/contactus"
@@ -159,16 +156,12 @@ export default function Navbar({ isLogin, setIsLogin }) {
                 >
                   <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
                 </svg>
-<<<<<<< HEAD
                 {cartItems.length > 0 && (
                   <span className={style.cartItemCount} 
                   onClick={() =>
                    fetchCartItems
                   }>{cartItems.length}</span>
-=======
-                {numOfitems> 0 && (
-                  <span className={style.cartItemCount}>{numOfitems}</span>
->>>>>>> main
+
                 )}
               </Link>
               </li>
